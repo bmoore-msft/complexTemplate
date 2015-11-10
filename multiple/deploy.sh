f227f26fe2e7b312d5641b19b4f2f796f308e147
Neil Gat <negat@microsoft.com>
2015-11-09 18:39:13 -0800

    multiple template works

--- multiple/deploy.sh
+++ multiple/deploy.sh
@@ -1,1 +1,1 @@
-azure group create -n "negatmultrg" -d "negatmultdep" -l "East US" -f multiple-vmss-ubuntu-vnet-multiple-storage-deploy.json -e parameters.json
+azure group create -n "negatmultirg" -d "negatmultidep" -l "East US" -f multiple-vmss-ubuntu-vnet-multiple-storage-deploy.json -e parameters.json
